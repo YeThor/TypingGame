46eb83f3b38e260947be74b7a7e9f71c6f2ce82e
YeThor <kysloves@naver.com>
2021-03-10 22:52:26 +0900

    :wrench: mini-css-extract-plugin 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -29,12 +31,20 @@ module.exports = {
         loader: "babel-loader",
         exclude: [/node_modules/],
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
   ],
 };
