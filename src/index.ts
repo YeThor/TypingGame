f97b1c8b4295af56919c104dc44cebc20b7401f2
YeThor <kysloves@naver.com>
2021-03-10 18:44:53 +0900

    :white_check_mark: 단위테스트 테스트용 파일 추가

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  sum(1, 2);
   let p = Promise.resolve(1);
   console.log(p);
   console.log("hi");
