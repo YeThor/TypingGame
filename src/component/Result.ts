79017a30c1fde69f02080955c21247228041859b
YeThor <kysloves@naver.com>
2021-03-11 20:58:42 +0900

    :sparkles: 결과화면 함수형 컴포넌트 추가

---
+++ src/component/Result.ts
@@ -0,0 +1,28 @@
+const Result = (
+  container: HTMLElement,
+  score: number,
+  average: number
+): void => {
+  const template = `
+    <div class="result">
+      <p class="greeting">Mission Complete!</p>
+      <p class="score">당신의 점수는 ${score}점입니다</p>
+      <p class="average">단어당 평균 답변 시간은 ${average}초입니다.</p>
+      <button id='restart-btn' class="game-btn">다시 시작</button>
+    </div>
+    `;
+
+  container.innerHTML = template;
+
+  const restartButton = document.getElementById("restart-btn");
+
+  if (!restartButton) {
+    throw new Error("No restart button");
+  }
+
+  restartButton.addEventListener("click", () => {
+    window.location.hash = "";
+  });
+};
+
+export default Result;
