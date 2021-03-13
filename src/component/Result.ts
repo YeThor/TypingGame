9c2af33a8388a995b0ddc8929483b9c78b24fdd5
YeThor <kysloves@naver.com>
2021-03-14 02:28:02 +0900

    :recycle: 클래스 메소드 일부를  순수함수로 변경

--- src/component/Result.ts
+++ src/component/Result.ts
@@ -6,27 +6,47 @@ export interface ResultData {
   average?: number;
 }
 
-const Result = (data: ResultData): HTMLElement => {
-  const { score = 0, average = 0 } = data;
-  const rootElement = document.createElement("div");
+class Result {
+  score: number;
+  average: number;
+  private _rootElement: HTMLElement;
 
-  rootElement.classList.add("result");
-  rootElement.innerHTML = `
+  constructor(data: ResultData) {
+    const { score = 0, average = 0 } = data;
+
+    this.score = score;
+    this.average = average;
+    this._rootElement = this.createDOM(this.score, this.average);
+    this.attachEvent(this._rootElement);
+  }
+
+  get rootElement(): HTMLElement {
+    return this._rootElement;
+  }
+
+  createDOM = (score: number, average: number): HTMLElement => {
+    const rootElement = document.createElement("div");
+
+    rootElement.classList.add("result");
+    rootElement.innerHTML = `
     <p class="greeting">Mission Complete!</p>
     <p class="score">당신의 점수는 ${score}점입니다</p>
     <p class="average">단어당 평균 답변 시간은 ${average}초입니다.</p>
     <button id='restart-btn' class="game-btn">다시 시작</button>
   `;
 
-  const restartButton = rootElement.querySelector("#restart-btn");
+    return rootElement;
+  };
 
-  if (!restartButton) throw new Error(ERROR_TYPE.NO_SUCH_ELEMENT);
+  attachEvent = (rootElement: HTMLElement): void => {
+    const restartButton = rootElement.querySelector("#restart-btn");
 
-  restartButton.addEventListener("click", () => {
-    window.location.hash = "";
-  });
+    if (!restartButton) throw new Error(ERROR_TYPE.NO_SUCH_ELEMENT);
 
-  return rootElement;
-};
+    restartButton.addEventListener("click", () => {
+      window.location.hash = "";
+    });
+  };
+}
 
 export default Result;
