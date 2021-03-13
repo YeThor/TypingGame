c4644f5aa6955e8705d582622799294804b01ea3
YeThor <kysloves@naver.com>
2021-03-14 02:23:41 +0900

    :fire: 샘플용으로 존재하던 테스트코드 삭제

--- test/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-import sum from "../src/sum";
-
-describe("index.ts 테스트", () => {
-  it("sum 테스트", () => {
-    expect(sum(1, 1)).to.equal(2);
-    expect(sum(1, 1)).to.not.equal("귀요미");
-  });
-});
